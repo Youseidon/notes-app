3efa3d5cc59468a5f7619c5834786197c6000b95
Yousef Nourizadeh <nou.yousef@gmail.com>
2020-05-16 23:02:13 +0400

    npm yargs added

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 const validator = require("validator");
 const chalk = require("chalk");
 const getNotes = require("./notes");
+const yargs = require('yargs');
 
-const msg = getNotes();
-console.log(msg);
-console.log(chalk.red(validator.isURL("gmail/.com")))
-const log = console.log;
-log(chalk.green.bold.inverse('Success') + ' Message ' + chalk.inverse.red("Error!"));
+// console.log(process.argv);
+//Customize yargs version
+yargs.version('12.0.1');
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note');
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log("Removing a note");
+    }
+});
+
+yargs.command({
+    command: 'read',
+    describe: 'Reading a note',
+    handler: function(arg) {
+        console.log("Reading a note", arg._[1]);
+    }
+});
+
+yargs.command({
+    command: 'list',
+    describe: 'Listing a note',
+    handler: function() {
+        console.log("Listing out all notes");
+    }
+});
+
+console.log(yargs.argv);
