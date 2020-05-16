aa7eab2eb54bc5cbcd011599791e40928c66db83
Yousef Nourizadeh <nou.yousef@gmail.com>
2020-05-16 23:04:33 +0400

    npm chalk added

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const validator = require("validator");
 const chalk = require("chalk");
 const getNotes = require("./notes");
 
 const msg = getNotes();
 console.log(msg);
-console.log(chalk.red(validator.isURL("gmail/.com")))
 const log = console.log;
 log(chalk.green.bold.inverse('Success') + ' Message ' + chalk.inverse.red("Error!"));
